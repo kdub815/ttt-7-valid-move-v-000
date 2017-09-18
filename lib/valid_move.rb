4988b83a4246f0e4996571bf0ab6d592a70a2604
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-09-18 22:42:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def valid_move?(board, index)
-    if index => 0 && index <= 8
+    if index < 0 && index > 8
       false
     elsif position_taken?
       false
